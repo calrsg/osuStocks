418e945de597143067275d30e3a9eb8977f03a69
Callum Sproul <sprcr001@mymail.unisa.edu.au>
2023-01-29 04:09:52 +1030

    Add placeholder for user profile cog and command(s)

---
+++ cogs/profile.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+class Profile(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command(name="me", with_app_command=True, description="Return user information such as balance, stock holdings, etc.")
+    @app_commands.guilds(discord.Object(id=833991086740996117))
+    async def who(self, ctx):
+        await ctx.send("Placeholder, will return user balance and stock holdings in a nice looking embed :)")
+
+
+async def setup(bot):
+    # take name of class, pass in the bot
+    await bot.add_cog(Profile(bot))
